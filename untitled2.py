fd45ce1b071c1439805e7a975198c5aff40b5834
Daniel OSSE <anonosse@gmail.com>
2020-10-12 19:47:29 +0200

    uploaded scraper and run code

---
+++ untitled2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 12:25:06 2020
+
+@author: Dani Ossé
+"""
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/ossed/MyDSProjects/Quant_salary_prediction/Quant_salary_prediction/chromedriver"
+df = gs.get_jobs('quantitative analyst',3,False,path,15)
